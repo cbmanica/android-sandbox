f7f043f9c495601c34995748fb83285a4e268de4
C. Benson Manica <cbmanica@gmail.com>
2012-08-07 00:13:51 -0700

    Add launcher icon, app header image, change app name, fix the title bar stuff

--- src/org/theuntaintedsky/helloworld/HelloWorld.java
+++ src/org/theuntaintedsky/helloworld/HelloWorld.java
@@ -1,15 +1,17 @@
 package org.theuntaintedsky.helloworld;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.app.*;
+import android.os.*;
+import android.view.*;
 
-public class HelloWorld extends Activity
-{
-    /** Called when the activity is first created. */
+public class HelloWorld extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
     }
 }
