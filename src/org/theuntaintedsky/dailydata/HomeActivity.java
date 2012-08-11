6cab2e452baae05aca7f926179aa39cb895fe1a0
C. Benson Manica <cbmanica@gmail.com>
2012-08-11 12:21:25 -0700

    WOOHOO I can make buttons that (kind of) LOOK like buttons!!

--- src/org/theuntaintedsky/dailydata/HomeActivity.java
+++ src/org/theuntaintedsky/dailydata/HomeActivity.java
@@ -1,39 +1,51 @@
 package org.theuntaintedsky.dailydata;
 
-import android.app.*;
-import android.content.*;
-import android.content.res.*;
 import android.database.*;
-import android.graphics.*;
 import android.os.*;
-import android.util.*;
+import android.support.v4.app.*;
+import android.support.v4.content.*;
 import android.view.*;
 import android.widget.*;
 import org.theuntaintedsky.dailydata.data.*;
 import org.theuntaintedsky.dailydata.data.table.*;
+import org.theuntaintedsky.dailydata.ui.*;
 
-public class HomeActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+public class HomeActivity extends android.app.Activity implements LoaderManager.LoaderCallbacks<Cursor> {
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return null;  // TODO
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        // TODO
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        // TODO
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
+
         final DatabaseManager manager = new DatabaseManager(this);
         manager.close();
+
+        // Create an empty adapter we will use to display the loaded data.
+//        mAdapter = new SimpleCursorAdapter(getActivity(),
+//                android.R.layout.simple_list_item_2, null,
+//                new String[]{},
+//                new int[]{android.R.id.text1, android.R.id.text2}, 0);
+        CursorLoader foo;
         final Cursor cursor = getContentResolver().query(DataDescriptor.URI, new String[]{DataDescriptor.Columns.DESCRIPTION}, null, null, null);
-        System.out.println("PORK 1 it worked?  Got back " + cursor.getCount());
-        final ContentValues values = new ContentValues();
+        final android.content.ContentValues values = new android.content.ContentValues();
         values.put(DataDescriptor.Columns.DESCRIPTION, "Hello, world!");
         getContentResolver().insert(DataDescriptor.URI, values);
         cursor.requery(); // sigh, they changed everything I knew...
-        System.out.println("PORK 2 it worked?  Got back " + cursor.getCount());
-        final AssetManager assetManager = getAssets();
-        final Typeface lg = Typeface.createFromAsset(assetManager, "league_gothic.ttf");
-        ((TextView) findViewById(R.id.hello_view)).setTypeface(lg);
-        ((TextView) findViewById(R.id.hello_view)).setTextSize(TypedValue.COMPLEX_UNIT_PT, 16);
-
+        TypefaceManager.applyTypeface(getAssets(), TypefaceManager.Types.DEFAULT_LARGE, (TextView) findViewById(R.id.hello_view));
     }
 }
---
+++ src/org/theuntaintedsky/dailydata/ui/TypefaceManager.java
@@ -0,0 +1,57 @@
+package org.theuntaintedsky.dailydata.ui;
+
+import android.content.res.*;
+import android.graphics.*;
+import android.util.*;
+import android.widget.*;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+/**
+ * <p/>
+ * This class highlights a major deficiency in the Android framework.
+ *
+ * @author <a href="mailto: C. Benson Manica <benson.manica@turner.com>">C. Benson Manica</a>
+ * @version $Revision: 1.0 Created 8/11/12 10:41 AM<br/> Copyright 2012 Turner Broadcasting, Inc.<br/> All
+ *          rights reserved.<br/>
+ */
+public class TypefaceManager {
+    public enum Types {
+        DEFAULT, DEFAULT_LARGE
+    }
+
+    private static final Map<Types, Typeface> hash = new ConcurrentHashMap<Types, Typeface>(new EnumMap<Types, Typeface>(Types.class));
+
+    public static Typeface getTypeface(AssetManager manager, Types type) {
+        Typeface ret = hash.get(type);
+        if (ret == null) {
+            if (manager == null) {
+                throw new IllegalStateException("Asset manager not passed and typeface not cached");
+            }
+            switch (type) {
+                case DEFAULT:
+                case DEFAULT_LARGE:
+                default:
+                    ret = Typeface.createFromAsset(manager, "league_gothic.ttf");
+                    break;
+            }
+            hash.put(type, ret);
+        }
+        return ret;
+    }
+
+    public static void applyTypeface(AssetManager manager, Types type, TextView textView) {
+        textView.setTypeface(getTypeface(manager, type));
+        switch (type) {
+            case DEFAULT:
+            default:
+                textView.setTextSize(TypedValue.COMPLEX_UNIT_PT, 10);
+                break;
+            case DEFAULT_LARGE:
+                textView.setTextSize(TypedValue.COMPLEX_UNIT_PT, 14);
+                break;
+        }
+    }
+}
+
